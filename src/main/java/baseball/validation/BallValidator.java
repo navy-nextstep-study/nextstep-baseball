5cf36c23a00600aeae3ef3de8b28f7241a924efa
bombo-dev <bombo96@chungwoon.ac.kr>
2023-06-17 01:04:17 +0900

    feat : 공 위치 범위 검증 기능 구현

--- src/main/java/baseball/validation/BallValidator.java
+++ src/main/java/baseball/validation/BallValidator.java
@@ -5,4 +5,8 @@ public class BallValidator {
     public static boolean validateNumberInRange(int number) {
         return number >= 1 && number <= 9;
     }
+
+    public static boolean validateIndexInRange(int index) {
+        return index >= 1 && index <= 3;
+    }
 }
