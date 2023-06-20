fe205cb23e08055109485fc6b094b27a3cbe008e
Yiseull <omjl5123@catholic.ac.kr>
2023-06-21 04:08:56 +0900

    refactor: 3개의 랜덤 숫자 생성 클래스명 변경, 패키지 이동

--- src/main/java/com/nextstep/baseball/util/BaseballNumbersGanerator.java
+++ src/main/java/com/nextstep/baseball/util/BaseballNumbersGanerator.java
@@ -1,18 +1,18 @@
-package com.nextstep.baseball.domain;
+package com.nextstep.baseball.util;
+
+import com.nextstep.baseball.domain.BaseballNumbers;
 
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
-public class Computer {
+public class BaseballNumbersGanerator {
 
     public static final int NUMBERS_SIZE = 3;
     public static final int MIN = 1;
     public static final int MAX = 9;
 
-    public static BaseballNumbers answer;
-
-    static {
-        answer = new BaseballNumbers(ThreadLocalRandom.current().ints(MIN, MAX + 1)
+    public BaseballNumbers generateRandomNumbers() {
+        return new BaseballNumbers(ThreadLocalRandom.current().ints(MIN, MAX + 1)
                 .distinct()
                 .limit(NUMBERS_SIZE)
                 .boxed()
