3e7797d8054a480d74e584dfc9bcf2ba2565e2ca
Yiseull <omjl5123@catholic.ac.kr>
2023-06-21 04:38:23 +0900

    feat: 숫자 야구 게임 실행 기능 추가

--- src/main/java/com/nextstep/baseball/controller/BaseballGameController.java
+++ src/main/java/com/nextstep/baseball/controller/BaseballGameController.java
@@ -1,11 +1,37 @@
 package com.nextstep.baseball.controller;
 
+import com.nextstep.baseball.domain.BaseballCounter;
+import com.nextstep.baseball.domain.BaseballNumbers;
 import com.nextstep.baseball.domain.BaseballStatus;
+import com.nextstep.baseball.util.BaseballNumbersGanerator;
+import com.nextstep.baseball.view.InputView;
+import com.nextstep.baseball.view.PrintView;
+
+import java.util.List;
 
 public class BaseballGameController {
 
     public void run() {
-
+        BaseballNumbers answers = BaseballNumbersGanerator.generateRandomNumbers();
+        while (true) {
+            try {
+                List<Integer> numbers = InputView.inputNumbers();
+                BaseballNumbers playerNumbers = new BaseballNumbers(numbers);
+                BaseballStatus status = BaseballCounter.compareNumbers(playerNumbers, answers);
+                PrintView.printBaseballStatus(status);
+                if (isGameEnd(status)) {
+                    PrintView.printGameEndMessage();
+                    PrintView.printMenu();
+                    int menu = InputView.inputMenu();
+                    if (!restartGame(menu)) {
+                        return;
+                    }
+                    answers = BaseballNumbersGanerator.generateRandomNumbers();
+                }
+            } catch (RuntimeException e) {
+                PrintView.printErrorMessage(e.getMessage());
+            }
+        }
     }
 
     public boolean isGameEnd(BaseballStatus status) {
