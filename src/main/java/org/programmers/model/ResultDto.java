b3c02839aee82b19c3d0efd224fdc48a51943d72
KimMinheee <99alsgmlalsl@naver.com>
2023-06-21 12:05:55 +0900

    [feat] : 누락된 ResultDto 클래스 추가

---
+++ src/main/java/org/programmers/model/ResultDto.java
@@ -0,0 +1,25 @@
+package org.programmers.model;
+
+public class ResultDto {
+    private final int ball;
+    private final int strike;
+
+    public ResultDto(int ball, int strike) {
+        this.ball = ball;
+        this.strike = strike;
+    }
+
+    public boolean is3Strike() {
+        return (this.strike == 3);
+    }
+
+    public int getBall() {
+        return ball;
+
+    }
+
+    public int getStrike() {
+        return strike;
+
+    }
+}
