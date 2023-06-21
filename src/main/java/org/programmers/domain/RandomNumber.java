86a67c4d1db23a1ef75cda69a5476c7088466c5d
KimMinheee <99alsgmlalsl@naver.com>
2023-06-21 14:14:46 +0900

    [feat] : 랜덤수 생성 테스트를 위한 validate 함수 추가

--- src/main/java/org/programmers/domain/RandomNumber.java
+++ src/main/java/org/programmers/domain/RandomNumber.java
@@ -4,23 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RandomNumber {
+    private static final int MAX_VALUE = 9;
+    private static final int MIN_VALUE = 1;
+
 
     //컴퓨터가 랜덤하게 수(3자리) 생성
     public static List<Integer> createRandomNumber() {
         List<Integer> numbers = new ArrayList<>();
 
         while (numbers.size() < 3) {
-            int randomNumber = (int) (Math.random() * 9) + 1;
+            int randomNumber = (int) (Math.random() * MAX_VALUE) + MIN_VALUE;
             if (!numbers.contains(randomNumber)) {
                 numbers.add(randomNumber);
             }
         }
 
-//        for (Integer i : numbers) {
-//            System.out.print(i);
-//        }
-
         return numbers;
+    }
+
+    //테스트를 위한 validate 함수
+
+    public static void validateRandomNumberDuplication(List<Integer> numbers) {
+        boolean hasDuplicate = numbers
+                .stream()
+                .distinct()
+                .count() != numbers.size();
+        if (hasDuplicate) {
+            throw new IllegalArgumentException("중복된 수가 존재합니다.");
+        }
+    }
+
+    public static void validateRange1To9(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < MIN_VALUE || number > MAX_VALUE) {
+                throw new IllegalArgumentException("1부터 9까지 범위의 수로 이루어져 있지 않습니다.");
+            }
+        }
     }
 
 
