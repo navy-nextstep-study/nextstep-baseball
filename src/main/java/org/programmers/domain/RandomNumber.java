b2fac169ad611a27b54f8c7629d478bc99ebcb9a
KimMinheee <99alsgmlalsl@naver.com>
2023-06-22 16:27:36 +0900

    [Refractor] : RandomNumber 클래스에서 3자리인지 확인하는 함수 생성 - NUMBER_SIZE 상수화

--- src/main/java/org/programmers/domain/RandomNumber.java
+++ src/main/java/org/programmers/domain/RandomNumber.java
@@ -6,13 +6,14 @@ import java.util.List;
 public class RandomNumber {
     private static final int MAX_VALUE = 9;
     private static final int MIN_VALUE = 1;
+    private static final int NUMBER_SIZE = 3;
 
 
     //컴퓨터가 랜덤하게 수(3자리) 생성
     public static List<Integer> createRandomNumber() {
         List<Integer> numbers = new ArrayList<>();
 
-        while (numbers.size() < 3) {
+        while (checkNumbersSize(numbers)) {
             int randomNumber = (int) (Math.random() * MAX_VALUE) + MIN_VALUE;
             if (!numbers.contains(randomNumber)) {
                 numbers.add(randomNumber);
@@ -20,6 +21,11 @@ public class RandomNumber {
         }
         return numbers;
     }
+
+    private static boolean checkNumbersSize(List<Integer> numbers) {
+        return numbers.size() == NUMBER_SIZE;
+    }
+
 
     //테스트를 위한 validate 함수
 
